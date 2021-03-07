5e283cd269cf498a4ac05347ed62d76d5b3aeb84
kir222k <u.k.send@gmail.com>
2021-03-07 23:05:48 +0300

    оптимизировано использование ресурсов

--- ac15/Classes/Init.cs
+++ ac15/Classes/Init.cs
@@ -1,5 +1,5 @@
 //  Файл инициализации библиотеки
-
+// https://overcoder.net/q/236774/%D0%B4%D0%BE%D0%B1%D0%B0%D0%B2%D0%B8%D1%82%D1%8C-resourcedictionary-%D0%B2-%D0%B1%D0%B8%D0%B1%D0%BB%D0%B8%D0%BE%D1%82%D0%B5%D0%BA%D1%83-%D0%BA%D0%BB%D0%B0%D1%81%D1%81%D0%BE%D0%B2
 // ПОЛЯ
 // СВОЙЙСТВА
 // КОНСТРУКТОРЫ
